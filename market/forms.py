041954f936afeb26593c93516b6e5a7ca19e982b
slowed <rootslowed@gmail.com>
2021-07-13 15:30:43 -0300

    register form using flask forms

---
+++ market/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+
+class RegisterForm(FlaskForm):
+  username = StringField(label='username:')
+  email = StringField(label='email:')
+  password1 = PasswordField(label='password:')
+  password2 = PasswordField(label='confirm password:')
+  submit = SubmitField(label='create account')
