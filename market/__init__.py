a045b355e6ac0a93c934754540d33fdd0f91319b
slowed <rootslowed@gmail.com>
2021-07-13 15:30:25 -0300

    secret key

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '7a06f7ccb51b674559b97c1a'
 db = SQLAlchemy(app)
 
 from market import routes
