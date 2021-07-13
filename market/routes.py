b9d998d64e05d740af7dc1480f10a557191df9fb
slowed <rootslowed@gmail.com>
2021-07-13 15:28:57 -0300

    register route

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 
 
 @app.route('/')
@@ -11,4 +12,10 @@ def home_page():
 @app.route('/market')
 def market_page():
   items = Item.query.all()
-  return render_template('market.html', items=items)
+  return render_template('market.html', items = items)
+
+
+@app.route('/register')
+def register_page():
+  form = RegisterForm()
+  return render_template('register.html', form = form)
