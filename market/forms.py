2a4e57ce875ed6d702da854766a5986ad0338f34
slowed <rootslowed@gmail.com>
2021-07-14 16:25:14 -0300

    form labels and validators

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-  username = StringField(label = 'username:')
-  email = StringField(label = 'email:')
-  password1 = PasswordField(label = 'password:')
-  password2 = PasswordField(label = 'confirm password:')
-  submit = SubmitField(label = 'create account')
+  username = StringField(
+    label='username:',
+    validators=[Length(min=2, max=30), DataRequired()],
+  )
+  
+  email = StringField(
+    label='email:',
+    validators=[Email(), DataRequired()],
+  )
+  
+  password1 = PasswordField(
+    label='password:',
+    validators=[Length(min=6), DataRequired()],
+  )
+  
+  password2 = PasswordField(
+    label='confirm password:',
+    validators=[EqualTo('password1'), DataRequired()],
+  )
+
+  submit = SubmitField(label='create account')
