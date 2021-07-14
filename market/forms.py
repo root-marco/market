6730c1378b76052d5aab70e38356e2b0dc5edc36
slowed <rootslowed@gmail.com>
2021-07-14 15:55:05 -0300

    register fields

--- market/forms.py
+++ market/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 
 
 class RegisterForm(FlaskForm):
-  username = StringField(label='username:')
-  email = StringField(label='email:')
-  password1 = PasswordField(label='password:')
-  password2 = PasswordField(label='confirm password:')
-  submit = SubmitField(label='create account')
+  username = StringField(label = 'username:')
+  email = StringField(label = 'email:')
+  password1 = PasswordField(label = 'password:')
+  password2 = PasswordField(label = 'confirm password:')
+  submit = SubmitField(label = 'create account')
