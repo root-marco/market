cc190ba0ee822d7af89319ca764d18a1a597eb25
slowed <rootslowed@gmail.com>
2021-07-16 13:45:37 -0300

    get password and set hashed passoword using bcrypt

--- market/models.py
+++ market/models.py
@@ -1,4 +1,4 @@
-from market import db
+from market import db, bcrypt
 
 
 class User(db.Model):
@@ -9,6 +9,14 @@ class User(db.Model):
   budget = db.Column(db.Integer(), nullable=False, default=1000)
   items = db.relationship('Item', backref='owned_user', lazy=True)
 
+  @property
+  def password(self):
+    return self.password
+
+  @password.setter
+  def password(self, plain_text_password):
+    self.password = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
 
 class Item(db.Model):
   id = db.Column(db.Integer(), primary_key=True)
@@ -17,6 +25,6 @@ class Item(db.Model):
   barcode = db.Column(db.String(length=12), nullable=False, unique=True)
   description = db.Column(db.String(length=1024), nullable=False, unique=True)
   owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
-  
+
   def __repr__(self):
     return f'Item {self.name}'
