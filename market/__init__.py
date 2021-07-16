387d5fd12f5231a864375117ded6b14b6a2a6851
slowed <rootslowed@gmail.com>
2021-07-16 13:29:02 -0300

    flask bcrypt imported

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '7a06f7ccb51b674559b97c1a'
+
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes
