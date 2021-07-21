20b04a5fed1921bf28015745a8d7583bf3114125
slowed <rootslowed@gmail.com>
2021-07-21 11:14:43 -0300

    login form with username, password and submit fields

--- market/forms.py
+++ market/forms.py
@@ -5,22 +5,36 @@ from market.models import User
 
 
 class RegisterForm(FlaskForm):
+
   def validate_username(self, username_param):
     user = User.query.filter_by(username=username_param.data).first()
-    if user:
-      raise ValidationError('username already exists')
+    if user: raise ValidationError('username already exists')
 
   def validate_email(self, email_param):
     email = User.query.filter_by(email=email_param.data).first()
-    if email:
-      raise ValidationError('email already exists')
+    if email: raise ValidationError('email already exists')
 
   username = StringField(label='username:',
     validators=[Length(min=2, max=30), DataRequired()])
+
   email = StringField(label='email:',
     validators=[Email(), DataRequired()])
+
   password1 = PasswordField(label='password:',
     validators=[Length(min=6), DataRequired()])
+
   password2 = PasswordField(label='confirm password:',
     validators=[EqualTo('password1'), DataRequired()])
-  submit = SubmitField(label='create account')
+
+  submit = SubmitField(label='sign up')
+
+
+class LoginForm(FlaskForm):
+  
+  username = StringField(label='username:',
+    validators=[DataRequired()])
+  
+  password = PasswordField(label='password:',
+    validators=[DataRequired()])
+
+  submit = SubmitField(label='sign in')
