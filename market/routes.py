dd276d6590af4c4813bb1ee44c6d3d37c5988368
slowed <rootslowed@gmail.com>
2021-07-21 11:14:58 -0300

    login route to handle post and get methods

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,7 @@
 from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 
 @app.route('/')
 def home_page():
@@ -34,3 +34,9 @@ def register_page():
       flash(error, category='danger')
 
   return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+  form = LoginForm()
+  return render_template('login.html', form=form)
