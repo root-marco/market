3d8df1d006cfabf71d576780972cd0f876e07a15
root-marco <rootslowed@gmail.com>
2021-07-24 12:19:57 +0000

    flask login with loginmanager imported and configured

--- market/__init__.py
+++ market/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = '7a06f7ccb51b674559b97c1a'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
