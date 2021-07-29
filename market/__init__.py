8a69eaa305746a35e662cffd70f31147f0c475f0
root-marco <rootslowed@gmail.com>
2021-07-29 14:21:30 -0300

    project done

--- market/__init__.py
+++ market/__init__.py
@@ -4,12 +4,11 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '7a06f7ccb51b674559b97c1a'
-
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -1,40 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from market.models import User
 
 
 class RegisterForm(FlaskForm):
 
-  def validate_username(self, username_param):
-    user = User.query.filter_by(username=username_param.data).first()
-    if user: raise ValidationError('username already exists')
+  def validate_username(self, username_to_check):
+    user = User.query.filter_by(username=username_to_check.data).first()
+    if user:
+      raise ValidationError('Username already exists! Please try a different username')
 
-  def validate_email(self, email_param):
-    email = User.query.filter_by(email=email_param.data).first()
-    if email: raise ValidationError('email already exists')
+  def validate_email_address(self, email_address_to_check):
+    email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+    if email_address:
+      raise ValidationError('Email Address already exists! Please try a different email address')
 
-  username = StringField(label='username:',
-    validators=[Length(min=2, max=30), DataRequired()])
-
-  email = StringField(label='email:',
-    validators=[Email(), DataRequired()])
-
-  password1 = PasswordField(label='password:',
-    validators=[Length(min=6), DataRequired()])
-
-  password2 = PasswordField(label='confirm password:',
-    validators=[EqualTo('password1'), DataRequired()])
-
-  submit = SubmitField(label='sign up')
+  username = StringField(label='User Name:', validators=[Length(min=2, max=30), DataRequired()])
+  email_address = StringField(label='Email Address:', validators=[Email(), DataRequired()])
+  password1 = PasswordField(label='Password:', validators=[Length(min=6), DataRequired()])
+  password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password1'), DataRequired()])
+  submit = SubmitField(label='Create Account')
 
 
 class LoginForm(FlaskForm):
-  
-  username = StringField(label='username:',
-    validators=[DataRequired()])
-  
-  password = PasswordField(label='password:',
-    validators=[DataRequired()])
+  username = StringField(label='User Name:', validators=[DataRequired()])
+  password = PasswordField(label='Password:', validators=[DataRequired()])
+  submit = SubmitField(label='Sign in')
 
-  submit = SubmitField(label='sign in')
+
+class PurchaseItemForm(FlaskForm):
+  submit = SubmitField(label='Purchase Item!')
+
+
+class SellItemForm(FlaskForm):
+  submit = SubmitField(label='Sell Item!')
